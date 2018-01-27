4ae18f44a3d378618e7370fec00ee0dd20727b92
sirslade <sirslade@gmail.com>
2018-01-27 13:01:50 -0800

    Add button  prefab and script, menu sprites

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour {
+
+    public Sprite downSprite;
+    public Sprite upSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spriteRenderer.sprite = downSprite;
+        }
+    }
+}
