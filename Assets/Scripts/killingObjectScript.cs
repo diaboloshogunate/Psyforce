f2d5b18eb16eb8ce5331eceea8515d6e958b80e2
sirslade <sirslade@gmail.com>
2018-01-27 21:55:08 -0800

    Break killing object into its own script

---
+++ Assets/Scripts/killingObjectScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killingObjectScript : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SendMessage("kill");
+            Debug.Log("Kill message sent");
+        }
+    }
+}
