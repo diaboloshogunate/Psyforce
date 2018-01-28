f5f9b1291aa6bf5dcfaf4977f5c346f9527c845d
Robert Parker <rparker@kodama.design>
2018-01-27 21:00:22 -0800

    change wall sprite, button uses ontrigger

--- Assets/Scripts/buttonScript.cs
+++ Assets/Scripts/buttonScript.cs
@@ -29,29 +29,21 @@ public class buttonScript : MonoBehaviour {
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Button " + gameObject.name + " Pushed");
-            int direction = 1;
-            if(!moveTargetUp) { direction = -1; }
-            foreach(GameObject o in m_targetObject)
+            isDown = true;
+            int direction = moveTargetUp ? 1 : -1;
+
+            foreach (GameObject o in m_targetObject)
             {
                 o.SendMessage("buttonPressed", direction);
             }
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-           isDown = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
