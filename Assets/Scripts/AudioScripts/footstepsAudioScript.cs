5fb7cf044b1c8fb9e640ee9b370d01f118794eb2
sirslade <sirslade@gmail.com>
2018-01-27 17:39:24 -0800

    Update audio stuff

---
+++ Assets/Scripts/AudioScripts/footstepsAudioScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstepsAudioScript : MonoBehaviour {
+    public AudioClip[] footstepClips;
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        Debug.Log("!!!! Got animator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(animator.GetCurrentAnimatorClipInfo(0).ToString());
+	}
+}
