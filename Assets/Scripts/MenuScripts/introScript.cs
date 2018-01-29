0d6ec4ed87af88f40751f1bbef740e0bf26f7068
sirslade <sirslade@gmail.com>
2018-01-28 16:13:40 -0800

    Update main menu

---
+++ Assets/Scripts/MenuScripts/introScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class introScript : MonoBehaviour {
+
+    public Canvas canvas;
+
+    private VideoPlayer vPlayer;
+    private VideoClip clip;
+   
+  
+
+    private void Awake()
+    {
+        vPlayer = GetComponent<VideoPlayer>();
+        vPlayer.loopPointReached += EndReached;
+    }
+
+    private void EndReached(VideoPlayer vPlayer)
+    {
+        canvas.gameObject.SetActive(true);
+    }
+}
