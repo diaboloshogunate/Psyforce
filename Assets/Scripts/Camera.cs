c6a99fb398b65879cf262e19fd0817b725a2435c
Robert Parker <rparker@kodama.design>
2018-03-29 17:26:34 -0700

    split screen yay!

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
 
-    public Transform[] targets;
+    public Transform target;
     public float smooth = 0.25f;
     private float targetX;
 
     void LateUpdate()
     {
-        float targetX = 0;
-        foreach (Transform target in targets)
-        {
-            targetX = Mathf.Max(targetX, target.position.x);
-        }
-        Vector3 moveTo = new Vector3(targetX, 0f, -20f);
+        Vector3 moveTo = new Vector3(target.position.x, target.position.y, -20f);
         Vector3 smothedPosition = Vector3.Lerp(transform.position, moveTo, smooth);
         transform.position = smothedPosition;
     }
